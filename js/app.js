0c99dc11576203b8ca000c685e85bc2771333446
anighosal <121676026+anighosal@users.noreply.github.com>
2023-03-02 21:21:32 +0600

    header section added

---
+++ js/app.js
@@ -0,0 +1,8 @@
+const loadAllData = () => {
+  const url = `https://jsonplaceholder.typicode.com/todos/1`;
+
+  fetch(url)
+    .then((response) => response.json())
+    .then((json) => console.log(json));
+};
+loadAllData();
