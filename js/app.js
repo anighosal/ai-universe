875eada53a5687d537034c7de27bf35411d3de36
anighosal <121676026+anighosal@users.noreply.github.com>
2023-03-02 22:38:18 +0600

    all image added

--- js/app.js
+++ js/app.js
@@ -10,7 +10,7 @@ const showAllData = (data) => {
   //   console.log(data);
   //   step-1 container element
   const toolsContainer = document.getElementById("tools-container");
-  data.tools.forEach((singleTool) => {
+  data.tools.slice(0, 6).forEach((singleTool) => {
     console.log(singleTool);
     // step-2 create child for each element
     const toolDiv = document.createElement("div");
@@ -19,7 +19,7 @@ const showAllData = (data) => {
     // step-3 set content of the child
     toolDiv.innerHTML = `
     <div class="card h-100">
-    <img src="..." class="card-img-top" alt="..." />
+    <img src="${singleTool.image}" class="card-img-top" alt="..." />
     <div class="card-body">
       <h5 class="card-title">Card title</h5>
       <p class="card-text">
