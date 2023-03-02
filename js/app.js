d219a55a3dc44d9fa689b87e9f76d22d8515eb6f
anighosal <121676026+anighosal@users.noreply.github.com>
2023-03-03 00:36:03 +0600

    arrow and date added

--- js/app.js
+++ js/app.js
@@ -19,14 +19,19 @@ const showAllData = (data) => {
     // step-3 set content of the child
     toolDiv.innerHTML = `
     <div class="card h-100 rounded-3">
-    <img src="${singleTool.image}" class="card-img-top p-3" alt="..." />
+    <img src="${singleTool.image}" class="card-img-top p-3 img-fluid" alt="..." />
     <div class="card-body">
       <h5 class="card-title">Features</h5>
       <small class="text-muted">1. Natural language processing</small> <br>
       <small class="text-muted">2. Contextual understanding</small> <br>
       <small class="text-muted">3. Text generation</small>
       <hr>
-      <h5 class="card-title">${singleTool.name}</h5>
+     <div class = "d-flex justify-content-between">
+        <div> <h5 class="card-title">${singleTool.name}</h5>
+        <small class="text-muted">11/01/2022</small></div>
+
+      <i class="fas fa-arrow-right mt-3" data-bs-toggle="modal"
+      data-bs-target="#exampleModal"></i></div>
     
       
       
