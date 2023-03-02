0318d0fc4547d9a85ca6b5a8188c358c431d1b5b
anighosal <121676026+anighosal@users.noreply.github.com>
2023-03-02 22:11:18 +0600

    all api data added

--- js/app.js
+++ js/app.js
@@ -1,8 +1,16 @@
 const loadAllData = () => {
-  const url = `https://jsonplaceholder.typicode.com/todos/1`;
+  const url = `https://openapi.programming-hero.com/api/ai/tools`;
 
   fetch(url)
     .then((response) => response.json())
-    .then((json) => console.log(json));
+    .then((data) => showAllData(data.data));
+};
+
+const showAllData = (data) => {
+  console.log(data);
+  data.tools.forEach((singleTool) => {
+    console.log(singleTool);
+    const toolDiv = document.createElement("div");
+  });
 };
 loadAllData();
