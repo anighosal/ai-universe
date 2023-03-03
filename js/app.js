0bd7bb7d7d824ef5f114ac2d8abea764ba941e99
anighosal <121676026+anighosal@users.noreply.github.com>
2023-03-03 20:56:09 +0600

    fresh code

--- js/app.js
+++ js/app.js
@@ -8,35 +8,35 @@ const loadAllData = () => {
 
 loadAllData();
 
-const showAllData = (tools) => {
+const showAllData = (allData) => {
   // console.log(tools);
   //   step-1 container element
 
   const toolsContainer = document.getElementById("tools-container");
 
-  tools.forEach((singleTool) => {
-    // console.log(singleTool);
-    // step-2 create child for each element
+  allData.forEach((singleTool) => {
+    console.log(singleTool);
+    //   // step-2 create child for each element
     const toolDiv = document.createElement("div");
     toolDiv.classList.add("col");
     // step-3 set content of the child
     toolDiv.innerHTML = `
-        <div class="card w-full h-96 rounded-3">
-        <img src="${singleTool.image}" class="card-img-top p-3 img-fluid" alt="..." />
-        <div class="card-body">
-          <h5 class="card-title">Features</h5>
-          <small class="text-muted">1. Natural language processing</small> <br>
-          <small class="text-muted">2. Contextual understanding</small> <br>
-          <small class="text-muted">3. Text generation</small>
-          <hr>
-         <div class = "d-flex justify-content-between">
-            <div> <h5 class="card-title">${singleTool.name}</h5>
-            <small class="text-muted">${singleTool.published_in}</small></div>
-            <i onClick="loadToolDetails('${singleTool.id}')" class="fas fa-arrow-right text-danger mt-4" data-bs-toggle="modal"
-            data-bs-target="#exampleModal"></i>
+          <div class="card w-full h-96 rounded-3">
+          <img src="${singleTool.image}" class="card-img-top p-3 img-fluid" alt="..." />
+          <div class="card-body">
+            <h5 class="card-title">Features</h5>
+            <small class="text-muted">1. Natural language processing</small> <br>
+            <small class="text-muted">2. Contextual understanding</small> <br>
+            <small class="text-muted">3. Text generation</small>
+            <hr>
+           <div class = "d-flex justify-content-between">
+              <div> <h5 class="card-title">${singleTool.name}</h5>
+              <small class="text-muted">${singleTool.published_in}</small></div>
+              <i onClick="loadToolDetails('${singleTool.id}')" class="fas fa-arrow-right text-danger mt-4" data-bs-toggle="modal"
+              data-bs-target="#exampleModal"></i>
+          </div>
         </div>
-      </div>
-        `;
+          `;
     toolsContainer.appendChild(toolDiv);
   });
 };
@@ -46,9 +46,8 @@ const showAllDataTogether = () => {
 
   fetch(url)
     .then((response) => response.json())
-    .then((data) => showAllData(data.data));
+    .then((data) => showAllData(data.data.tools));
 };
-showAllDataTogether();
 
 const toggleSpinner = (isLoading) => {
   const loaderSection = document.getElementById("loader");
