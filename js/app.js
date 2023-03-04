8d6980c8d72ba7e2150c88dc8616c9d14255a6a9
anighosal <121676026+anighosal@users.noreply.github.com>
2023-03-04 10:56:35 +0600

    all data added

--- js/app.js
+++ js/app.js
@@ -8,15 +8,23 @@ const loadAllData = () => {
 
 loadAllData();
 
-const showAllData = (allData) => {
-  // console.log(tools);
+const showAllDataTogether = () => {
+  const url = ` https://openapi.programming-hero.com/api/ai/tools`;
+
+  fetch(url)
+    .then((response) => response.json())
+    .then((data) => showAllData(data.data.tools));
+};
+
+const showAllData = (tools) => {
+  console.log(tools);
   //   step-1 container element
 
   const toolsContainer = document.getElementById("tools-container");
   toolsContainer.innerHTML = "";
 
-  allData.forEach((singleTool) => {
-    console.log(singleTool);
+  tools.forEach((singleTool) => {
+    // console.log(tool);
     //   // step-2 create child for each element
     const toolDiv = document.createElement("div");
     toolDiv.classList.add("col");
@@ -33,7 +41,7 @@ const showAllData = (allData) => {
            <div class = "d-flex justify-content-between">
               <div> <h5 class="card-title">${singleTool.name}</h5>
               <small class="text-muted">${singleTool.published_in}</small></div>
-              <i onClick="loadToolDetails('${singleTool.id}')" class="fas fa-arrow-right text-danger mt-4" data-bs-toggle="modal"
+              <i onclick="loadToolDetails('${singleTool.id}')" class="fas fa-arrow-right text-danger mt-4" data-bs-toggle="modal"
               data-bs-target="#exampleModal"></i>
           </div>
         </div>
@@ -42,65 +50,36 @@ const showAllData = (allData) => {
   });
 };
 
-const showAllDataTogether = () => {
-  const url = ` https://openapi.programming-hero.com/api/ai/tools`;
-
-  fetch(url)
-    .then((response) => response.json())
-    .then((data) => showAllData(data.data.tools));
-};
-showAllData();
-
-const toggleSpinner = (isLoading) => {
-  const loaderSection = document.getElementById("loader");
-  if (isLoading) {
-    loaderSection.classList.remove("d-none");
-  } else {
-    loaderSection.classList.add("d-none");
-  }
-};
-
-// no the best way to load show all
-// document.getElementById("btn-show-all").addEventListener("click", function () {
-//   toggleSpinner(true);
-// });
-
-// modal
 const loadToolDetails = (id) => {
-  // console.log(id);
   const url = `https://openapi.programming-hero.com/api/ai/tool/01`;
-  // console.log(url);
+  console.log(url);
   fetch(url)
     .then((res) => res.json())
     .then((data) => showToolDetails(data.data));
 };
+loadToolDetails();
 
 const showToolDetails = (tool) => {
-  // console.log(tool);
+  console.log(tool);
   const modalTitle = document.getElementById("exampleModalLabel");
   modalTitle.innerText = tool.tool_name;
 
   document.getElementById("modal-body").innerHTML = `
 
-<div class= "d-lg-flex w-fullgap-1 ">
+<div class= "d-lg-flex w-full h-100 gap-1 ">
 
 <div class="card bg-danger-subtle w-100 ">
 <h5 class= "p-2">${tool.description}</h5>
 <div class="card-body row">
 <div class="row row-cols-lg-3 gap-2">
 
-<p class="card-text bg-body w-[132px] rounded fw-semibold text-center text-success col ">${
-    tool.pricing[0].price ? tool.pricing[0].price : "free of cost basic"
-  }<br>${tool.pricing[0].plan}</p>
 
-<p class="card-text bg-body rounded fw-semibold text-warning col">${
-    tool.pricing[1].price
-  }<br>${tool.pricing[1].plan}</p>
+<p class="card-text bg-body w-[132px] rounded fw-semibold text-center text-success col ">${tool.pricing[0].price}<br>${tool.pricing[0].plan}</p>
+
+<p class="card-text bg-body rounded fw-semibold text-warning col">${tool.pricing[1].price}<br>${tool.pricing[1].plan}</p>
 
 
-<p class="card-text bg-body rounded fw-semibold text-center text-danger col">${
-    tool.pricing[2].price
-  }${tool.pricing[2].plan}</p>
+<p class="card-text bg-body rounded fw-semibold text-center text-danger col">${tool.pricing[2].price}${tool.pricing[2].plan}</p>
 
 </div>
  
@@ -123,12 +102,8 @@ const showToolDetails = (tool) => {
 <img class="card-img-top p-3 img-fluid" src="${tool.image_link[1]}
 " alt="" srcset="">
 <div class="card-body">
-  <h6 class="card-text text-black text-center">${
-    tool.input_output_examples[0].input
-  }</h6>
-  <small class="card-text text-black text-center">${
-    tool.input_output_examples[0].output
-  }</small>
+  <h6 class="card-text text-black text-center">${tool.input_output_examples[0].input}</h6>
+  <small class="card-text text-black text-center">${tool.input_output_examples[0].output}</small>
   
 </div>
 </div>
@@ -140,10 +115,18 @@ const showToolDetails = (tool) => {
      `;
 };
 
-// modal end
-// document.getElementById();
-// document
-//   .getElementById("btn-show-all")
-//   .addEventListener("click", function () {}); */
+const toggleSpinner = (isLoading) => {
+  const loaderSection = document.getElementById("loader");
+  if (isLoading) {
+    loaderSection.classList.remove("d-none");
+  } else {
+    loaderSection.classList.add("d-none");
+  }
+};
 
-loadToolDetails();
+// no the best way to load show all
+document.getElementById("btn-show-all").addEventListener("click", function () {
+  toggleSpinner(true);
+});
+
+// modal
